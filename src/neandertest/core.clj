211aac1594d31e15c6e7924952f524e62e105b3f
tom <tom@that-place>
2020-08-05 09:58:32 -0500

    Specified import order seems to work.

--- src/neandertest/core.clj
+++ src/neandertest/core.clj
@@ -1,10 +1,14 @@
-(ns neandertest.core
-  (:require [uncomplicate.neanderthal
-             [native :refer [dv dge fv fge native-float]]
-             [core :refer [submatrix native dot mm ge]]
-             [math :refer [sqrt log sin pi sqr]]
-             [random :refer [rand-normal! rand-uniform! rng-state]]])
-  (:import org.bytedeco.mkl.global.mkl_rt))
+(ns neandertest.core)
+;;for some reason, we currently have to manually enforce the
+;;import and require order, or else neanderthal can't find the
+;;native deps.  E.g., putting this in a namespace doesn't work.
+(import   'org.bytedeco.mkl.global.mkl_rt)
+(require '[uncomplicate.neanderthal
+           [native :refer [dv dge fv fge native-float]]
+           [core :refer [submatrix native dot mm ge]]
+           [math :refer [sqrt log sin pi sqr]]
+           [random :refer [rand-normal! rand-uniform! rng-state]]])
+
 
 (comment 
 (def x (dv 1 2 3))
